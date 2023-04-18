a5a06d017018ea5e2990d2a2071b9d792e5290d8
Kingsley Chimezie <kingsley.chimezie@genesys.com>
2023-04-18 16:44:12 +0100

    added client ID and issuer

--- require-auth/src/config.js
+++ require-auth/src/config.js
@@ -1,5 +1,5 @@
-const CLIENT_ID = process.env.CLIENT_ID || '{clientId}';
-const ISSUER = process.env.ISSUER || 'https://{yourOktaDomain}/oauth2/default';
+const CLIENT_ID = process.env.CLIENT_ID || 0oa7k60d79GoIPmmo5d7;
+const ISSUER = process.env.ISSUER || 'https://dev-6732777.okta.com/oauth2/default';
 const OKTA_TESTING_DISABLEHTTPSCHECK = process.env.OKTA_TESTING_DISABLEHTTPSCHECK || false;
 const REDIRECT_URI = `${window.location.origin}/login/callback`;
 const POST_LOGOUT_REDIRECT_URI = `${window.location.origin}/logout/callback`
