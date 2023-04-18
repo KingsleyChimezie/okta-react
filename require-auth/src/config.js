0d2bc3df4967e4311bc95a5eab4a6b43a0622d89
Kingsley Chimezie <kingsley.chimezie@genesys.com>
2023-04-18 16:45:25 +0100

    added quotes

--- require-auth/src/config.js
+++ require-auth/src/config.js
@@ -1,4 +1,4 @@
-const CLIENT_ID = process.env.CLIENT_ID || 0oa7k60d79GoIPmmo5d7;
+const CLIENT_ID = process.env.CLIENT_ID || '0oa7k60d79GoIPmmo5d7';
 const ISSUER = process.env.ISSUER || 'https://dev-6732777.okta.com/oauth2/default';
 const OKTA_TESTING_DISABLEHTTPSCHECK = process.env.OKTA_TESTING_DISABLEHTTPSCHECK || false;
 const REDIRECT_URI = `${window.location.origin}/login/callback`;
